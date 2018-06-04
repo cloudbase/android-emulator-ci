22f76c682f17d750262fca3877743251e784bb67
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2018-06-04 19:26:14 +0300

    Update 'run_tests' job

--- jobs/run_tests.sh
+++ jobs/run_tests.sh
@@ -6,6 +6,9 @@ SCRIPT_DIR=$(dirname "$BASH_SOURCE")
 
 source "$SCRIPT_DIR/job.rc"
 source "$SCRIPT_DIR/../utils/linux/all.sh"
+source "$SCRIPT_DIR/utils.sh"
 
+log_summary "Running emulator tests."
+start_emu_vm_job "run_tests"
 
 mark_job_completed "run_tests"
