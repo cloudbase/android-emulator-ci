721d2d154c51826851b369e3953db6b9f88fca6a
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2018-06-05 10:05:41 +0300

    Transfer unit tests archive

--- jobs/run_tests.sh
+++ jobs/run_tests.sh
@@ -8,7 +8,14 @@ source "$SCRIPT_DIR/job.rc"
 source "$SCRIPT_DIR/../utils/linux/all.sh"
 source "$SCRIPT_DIR/utils.sh"
 
+ps_emu_vm "Invoke-WebRequest -Uri $ANDROID_SDK_URL" \
+          "-OutFile $EMU_VM_SDK_TOOLS_PATH"
+
+log_summary "Transfering the unit tests archive."
+ps_emu_vm "Invoke-WebRequest -Uri $UNITTESTS_ARCHIVE_URL" \
+          "-OutFile $EMU_VM_UNITTESTS_ARCH_PATH"
+
 log_summary "Running emulator tests."
-start_emu_vm_job "run_tests"
+start_emu_vm_job "run_tests" "$EMU_VM_UNITTESTS_ARCH_PATH"
 
 mark_job_completed "run_tests"
